882a1ee51f1525e717c6f5b7633612b9e14e9d96
Alvin Vrolijk <alvin@funenzo.yt>
2020-01-24 18:30:07 +0100

    ArrowPickup v1.0

--- src/main/java/nl/alvinvrolijk/arrowpickup/ArrowPickup.java
+++ src/main/java/nl/alvinvrolijk/arrowpickup/ArrowPickup.java
@@ -3,7 +3,6 @@ package nl.alvinvrolijk.arrowpickup;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
